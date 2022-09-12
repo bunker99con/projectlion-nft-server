6ad84a027175f414055f0eba422a9c5f640d5708
goldhanwool <70783496+goldhanwool@users.noreply.github.com>
2022-09-12 18:08:21 +0900

    Delete nodeJS/src directory

--- nodeJS/src/model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    address: { type: String, required: true },
-    point: Number,
-}, {timestamps: true})
-
-const User = mongoose.model('user', UserSchema)
-module.exports = {User}
